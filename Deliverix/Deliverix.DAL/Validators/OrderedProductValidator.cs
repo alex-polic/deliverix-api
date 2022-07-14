d9d6f5c01e1cc9625d0bbd18f666a893e6e155d6
aleksandar-polic <alpolic8@gmail.com>
2022-07-14 23:30:44 +0200

    Fix Ordered Product validator.

--- Deliverix/Deliverix.DAL/Validators/OrderedProductValidator.cs
+++ Deliverix/Deliverix.DAL/Validators/OrderedProductValidator.cs
@@ -8,7 +8,7 @@ public class OrderedProductValidator : AbstractValidator<OrderedProduct>
     public OrderedProductValidator()
     {
         RuleFor(e => e.OrderId).NotNull().WithMessage("Order is mandatory field");
-        RuleFor(e => e.Product).NotNull().WithMessage("Product is mandatory field");
+        RuleFor(e => e.ProductId).NotNull().WithMessage("Product is mandatory field");
         RuleFor(e => e.Amount).NotNull().GreaterThan(0).WithMessage("Amount is mandatory field");
     }
 }
